a320d28877effc9c19cb18d849724b651cb4717d
Pankaj Kumar Bind <pankajbind30@gmail.com>
2023-08-11 20:43:07 +0530

    final

---
+++ script.js
@@ -0,0 +1,18 @@
+const loginForm = document.getElementById('loginForm');
+
+loginForm.addEventListener('submit', function (event) {
+  event.preventDefault();
+
+  const id = document.getElementById('id').value;
+  const password = document.getElementById('password').value;
+
+  // Replace these values with your actual ID and password
+  const validId = '9506608157';
+  const validPassword = 'hello';
+
+  if (id === validId && password === validPassword) {
+    window.location.href = 'success.html';
+  } else {
+    alert('Invalid ID or password. Please try again.');
+  }
+});
